fb9c2f21e87711623705a4db96339f9242835425
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-12-05 16:51:49 +0000

    Move to new folder

---
+++ build.mjs
@@ -0,0 +1,35 @@
+import { readdirSync, statSync } from "fs";
+import { exec } from "child_process";
+
+const ignoreddirs = ["node_modules", ".git"];
+
+// iterate over directories in the current directory
+readdirSync(__dirname).forEach((dir, i, dirs) => {
+	// ignore files
+	if (statSync(dir).isFile()) {
+		console.log(`Skipping ${dir} because it is a file (${i + 1}/${dirs.length})`);
+		return;
+	}
+
+	// ignore certain dirs
+	if (dir in ignoreddirs) {
+		console.log(`Skipping ${dir} (${i + 1}/${dirs.length})`);
+		return;
+	}
+
+	// if the dir has a package.json, build it
+	if (statSync(`${dir}/package.json`).isFile()) {
+		console.log(`Building ${dir} (${i + 1}/${dirs.length})`);
+		// run a console command
+		exec(`cd ${dir} && npm run build`, (err, stdout, stderr) => {
+			if (err) {
+				console.err(err);
+				return;
+			} else if(stderr) {
+				console.err(stderr);
+				return;
+			}
+			console.log(stdout);
+		});
+	}
+});
