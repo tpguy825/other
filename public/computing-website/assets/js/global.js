e40e21c710f2fedbc855cb1d3aafd6aeeca8e713
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-12-13 18:39:44 +0000

    speed

--- public/computing-website/assets/js/global.js
+++ public/computing-website/assets/js/global.js
@@ -1,8 +1,3 @@
 window.onerror = function (message, url, line, col) {
 	alert(`Error: ${message} at ${url}:${line}:${col}`);
 };
-
-const path = new URL(window.location.href).pathname;
-if (!path.endsWith("computing-website/") && path.endsWith("computing-website")) {
-	window.location.replace(window.location.href + "/");
-}
