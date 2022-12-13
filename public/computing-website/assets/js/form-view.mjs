94740dca28f12c8419b1c9c846dcb09741612f43
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-12-13 18:47:53 +0000

    haha refresh

--- public/computing-website/assets/js/form-view.mjs
+++ public/computing-website/assets/js/form-view.mjs
@@ -20,38 +20,38 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-/** @param {HTMLTableElement} table */
-async function gentable(table) {
-	const snapshot = await get(ref(database));
-	if (snapshot.exists()) {
-		const data = snapshot.val();
-		table.innerHTML = `<thead><tr><th>ID</th><th>Name</th><th>Suggestion</th><th>Edit</th></tr></thead><tbody></tbody>`
-		for (const key in data) {
-			const row = table.insertRow();
-			row.insertCell().innerText = key;
-			row.insertCell().innerText = data[key].name;
-			row.insertCell().innerText = data[key].suggestion;
+/**
+ * @param {HTMLTableElement} table
+ * @param {{ name: string, suggestion: string }[]} data
+ */
+async function gentable(data, table) {
+	table.innerHTML = `<thead><tr><th>ID</th><th>Name</th><th>Suggestion</th><th>Edit</th></tr></thead><tbody></tbody>`;
+	for (const key in data) {
+		const row = table.insertRow();
+		row.insertCell().innerText = key;
+		row.insertCell().innerText = data[key].name;
+		row.insertCell().innerText = data[key].suggestion;
 
-			const deletetc = row.insertCell();
-			const deletebutton = document.createElement("button");
+		const deletetc = row.insertCell();
+		const deletebutton = document.createElement("button");
 
-			deletebutton.className = "btn btn-danger";
-			deletebutton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>`;
-			deletebutton.onclick = () => deleteSuggestion(key);
+		deletebutton.className = "btn btn-danger";
+		deletebutton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>`;
+		deletebutton.onclick = () => deleteSuggestion(key);
 
-			deletetc.appendChild(deletebutton);
-		}
-	} else {
-		console.log("No data available");
+		deletetc.appendChild(deletebutton);
 	}
 }
 
+/**
+ * @param {string} key
+ */
 function deleteSuggestion(key) {
 	set(ref(database, key), null);
 	gentable(document.getElementById("table"));
 }
 
-gentable(document.getElementById("table"));
-setInterval(() => {
-	gentable(document.getElementById("table"));
-}, 5000)
+onValue(ref(database), (snapshot) => {
+	const data = snapshot.val();
+	gentable(data, document.querySelector("table"));
+});
