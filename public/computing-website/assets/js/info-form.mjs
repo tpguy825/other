1a62494e2f0eb8490315a649d0fc8f46550cd92c
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-12-14 16:30:34 +0000

    Formatting

--- public/computing-website/assets/js/info-form.mjs
+++ public/computing-website/assets/js/info-form.mjs
@@ -18,11 +18,7 @@ if (dom.form === null || dom.suggestion === null || dom.name === null || dom.sub
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
 import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: "AIzaSyBYIxtA8sF3-MoZNPvKoYCzs9XkgvTM_o8",
 	authDomain: "computing-unit-14-website.firebaseapp.com",
